291793e767a8cc4e898bc75c53b0491fd7b9ec38
Sam M. Vareka <84973673+Ponchoru-recur@users.noreply.github.com>
2025-06-28 13:35:54 +0800

    Save
    Testing new shading abilities with functions to create multiple lights (testing phase)

--- shaders/testFrag.fs
+++ shaders/testFrag.fs
@@ -8,6 +8,26 @@ struct Material {
     float shininess;
 };
 
+struct DirLight {
+    vec3 direction;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+struct PointLight {
+    vec3 position;
+
+    float constant;
+    float linear;
+    float quadratic;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
 struct Light {
     vec3 position;
     vec3 direction;
@@ -23,18 +43,25 @@ struct Light {
     float quadratic;
 };
 
-in vec2 texCoords;
-
+in vec2 texCoords;            // Texture Coordinates
 in vec3 vertexPositionWorld;  // Position of the object with transform
 in vec3 normalWorld;          // Normals of the object with transform
 uniform vec3 viewPosition;    // Camera Position
 
 uniform Material material;
 uniform Light light;
+uniform DirLight dirLight;
 
 uniform sampler2D emissionMap;
 
+#define NR_POINT_LIGHTS 4
+uniform PointLight pointLights[NR_POINT_LIGHTS];
+
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir);
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
+
 // TODO: DONT FORGET TO ADD VALUES TO THE LIGHTS AND CHANGE LightColor
+vec2 flipped = vec2(texCoords.x, 1.0 - texCoords.y);
 
 void main() {
     // Distance
@@ -42,7 +69,7 @@ void main() {
     float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
 
     // Texture
-    vec2 flipped = vec2(texCoords.x, 1.0 - texCoords.y);
+
     // Emission that i dont even use yet
     vec3 emission = texture(emissionMap, flipped).rgb * 1.0;
     // Ambient
@@ -72,4 +99,40 @@ void main() {
     // ambient + diffuse * texture
     vec3 result = (ambient + diffuse + specular);
     FragColor = vec4(result, 1.0);
+}
+
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir) {
+    vec3 lightDir = normalize(-light.direction);
+    // diffuse shading
+    float diff = max(dot(normal, lightDir), 0.0);
+    // specular
+    vec3 reflectorDir = reflect(-lightDir, normal);
+
+    float spec = pow(max(dot(viewDir, reflectorDir), 0.0), material.shininess);
+
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, flipped));
+    vec3 diffuse = light.diffuse * vec3(texture(material.diffuse, flipped));
+    vec3 specular = light.specular * vec3(texture(material.specular, flipped));
+    return (ambient + diffuse + specular);
+}
+
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir) {
+    vec3 lightDir = normalize(light.position - fragPos);
+    // Diffuse shading
+    float diff = max(dot(normal, lightDir), 0.0);
+    // Specular shading
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    // Attenuation - Basically a distance math wizardy so it doesnt light up something far away
+    float distance = length(light.position - fragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+
+    // Combine everything
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, flipped));
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, flipped));
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, flipped));
+    ambient *= attenuation;
+    diffuse *= attenuation;
+    specular *= attenuation;
+    return (ambient + diffuse + specular);
 }
