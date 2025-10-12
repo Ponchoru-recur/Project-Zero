a29ce2f1de4d88ae91edada7e4b432e3c162566f
Sam M. Vareka <84973673+Ponchoru-recur@users.noreply.github.com>
2025-10-12 21:06:57 +0800

    Nothing new

--- shaders/stencilFragShader.fs
+++ shaders/stencilFragShader.fs
@@ -8,5 +8,5 @@ in vec2 UV;
 void main() {
     vec3 col = texture(screenTex, UV).rgb;
     float grey = dot(col, vec3(0.299, 0.587, 0.114));
-    FragColor = vec4(vec3(grey), 1.0);
+    FragColor = vec4(vec3(1.0 - texture(screenTex, UV)), 1.0);
 }
